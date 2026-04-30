2f78e3ad8100410cbc8b8def81e48ddd4fe16648
Carlos Octavio Otero <166446069+OteroCarlos1977@users.noreply.github.com>
2026-04-30 16:06:09 -0300

    Clean Turnero Hospital deploy target

--- index.js
+++
@@ -1,8 +0,0 @@
-//Importar desd app.js.
-
-const app = require('./app');
-
-//Inicializar el servidor
-app.listen(app.get('port'), () => {
-  console.log('Servidor corriendo en el puerto', app.get('port'));
-});
--- modulos/disponibilidad_medica/index.js
+++
@@ -1,4 +0,0 @@
-const db = require('../../DB/database');
-const ctrl = require('./controlador');
-
-module.exports = ctrl(db);
